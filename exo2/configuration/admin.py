9f80e1606c99cd31f96ec746241e99397b9f90bc
franck <franckassovie12345@gmail.com>
2020-03-17 17:42:57 +0000

    update

--- exo2/configuration/admin.py
+++ exo2/configuration/admin.py
@@ -1,10 +1,121 @@
 from django.contrib import admin
 
 from . import models
-# Register your models here.
-admin.site.register(models.SocialAccount)
-admin.site.register(models.InfoSite)
-admin.site.register(models.Gallerie)
-admin.site.register(models.Temoignage)
-admin.site.register(models.Presentation)
 
+from django.utils.safestring import mark_safe
+
+
+
+# Register your models here.
+class SocialAccountAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Presentation',{'fields': ['nom']}),
+        ('reseaux', {'fields': ['icon','lien','status']})
+        
+    ]
+    
+    
+    list_display = ('nom','date_add','status')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 10
+    
+    
+class InfoSiteAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','titre']}),
+        ('Status',{'fields': ['site_map','status']})
+       
+    ]
+    
+    list_display = ('titre','date_add','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('titre',)
+    date_hierarchy = "date_add"
+    list_display_links = ['titre']
+    ordering = ['titre']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+
+class GallerieAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','titre']}),
+        ('Status',{'fields': ['status']})
+       
+    ]
+    
+    list_display = ('titre','date_add','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('titre',)
+    date_hierarchy = "date_add"
+    list_display_links = ['titre']
+    ordering = ['titre']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+
+class TemoignageAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','nom','prenom']}),
+        ('Status',{'fields': ['message','status']})
+       
+    ]
+    
+    list_display = ('nom','date_add','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+                                                                                                                                  
+    
+class PresentationAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','nom']}),
+        ('Status',{'fields': ['description','status']})
+       
+    ]
+    
+    list_display = ('nom','date_add','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+   
+
+
+    
+def _register(model,Admin_class):
+    admin.site.register(model,Admin_class)
+
+_register(models.SocialAccount, SocialAccountAdmin)  
+_register(models.Temoignage, TemoignageAdmin) 
+_register(models.Gallerie, GallerieAdmin) 
+_register(models.Presentation, PresentationAdmin) 
+_register(models.InfoSite, InfoSiteAdmin) 
+
+
+ 
--- exo2/prestation/admin.py
+++ exo2/prestation/admin.py
@@ -1,6 +1,55 @@
 from django.contrib import admin
 
 from . import models
-# Register your models here.
-admin.site.register(models.Suite)
-admin.site.register(models.Service)
+
+from django.utils.safestring import mark_safe
+
+class SuiteAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','nom']}),
+        ('Status',{'fields': ['prix','status']})
+       
+    ]
+    
+    list_display = ('nom','date_add','prix','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+                                                                                                                                  
+    
+class ServiceAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        ('Presentation',{'fields': ['image','nom']}),
+        ('Status',{'fields': ['status']})
+       
+    ]
+    
+    list_display = ('nom','date_add','status','image_views')
+    list_filter = ('status',)
+    search_fields = ('nom',)
+    date_hierarchy = "date_add"
+    list_display_links = ['nom']
+    ordering = ['nom']
+    list_per_page = 10
+    
+    def image_views(self,obj):
+        return mark_safe("<img src='{url}' width= 100px height=50px >".format(url=obj.image.url))
+
+   
+
+
+    
+def _register(model,Admin_class):
+    admin.site.register(model,Admin_class)
+
+_register(models.Service, ServiceAdmin)  
+_register(models.Suite, SuiteAdmin)
